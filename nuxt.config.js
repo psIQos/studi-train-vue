6e4ebc2fbcafcb1dc8052c78c5538ec44d03350d
Julian Mathis <julianmathis95@gmail.com>
2020-02-11 10:31:34 +0100

    installed nuxt-helmet

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,6 +48,7 @@ export default {
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     '@nuxtjs/pwa',
+    'nuxt-helmet',
     // Doc: https://github.com/nuxt-community/dotenv-module
     '@nuxtjs/dotenv',
     [
